cfd49b9b26675e7646a652638977c52dd927a07c
azjezz <sgmati48@gmail.com>
2018-10-13 23:00:24 +0100

    Update FunctionalTest.php

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -6,12 +6,24 @@ use ApiClients\Tools\TestUtilities\TestCase;
 use Test\App\Commands\AwesomesauceCommand;
 use Test\App\Handlers\AwesomesauceHandler;
 use function WyriHaximus\listClassesInDirectory;
+use function WyriHaximus\listClassesInDirectories;
 
 final class FunctionalTest extends TestCase
 {
     public function testListClassesInDirectory()
     {
         $classes = iterator_to_array(listClassesInDirectory(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'test-app' . DIRECTORY_SEPARATOR));
+        self::assertSame([
+            AwesomesauceCommand::class,
+            AwesomesauceHandler::class,
+        ], $classes);
+    }
+    
+    public function testListClassesInDirectories()
+    {
+        $app = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'test-app' . DIRECTORY_SEPARATOR;
+        
+        $classes = iterator_to_array(listClassesInDirectories($app . 'Handlers', $app . 'Commands'));
         self::assertSame([
             AwesomesauceCommand::class,
             AwesomesauceHandler::class,
