5365f38ba630a675dffe40ff679e13ab2b2f0b90
azjezz <sgmati48@gmail.com>
2018-10-13 22:52:38 +0100

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -9,13 +9,19 @@ use Roave\BetterReflection\SourceLocator\Type\AutoloadSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\DirectoriesSourceLocator;
 
 /**
+ * get a list of all classes in the given direcotories.
+ *
  * Based on: https://github.com/Roave/BetterReflection/blob/396a07c9d276cb9ffba581b24b2dadbb542d542e/demo/parsing-whole-directory/example2.php.
+ * 
+ * @param  array<int, string>  $directories
+ *
+ * @return iterable
  */
-function listClassesInDirectory(string $directory): iterable
+function listClassesInDirecories(string ...$directories): iterable
 {
     $sourceLocator = new AggregateSourceLocator([
         new DirectoriesSourceLocator(
-            [$directory],
+            $directories,
             (new BetterReflection())->astLocator()
         ),
         // ↓ required to autoload parent classes/interface from another directory than /src (e.g. /vendor)
@@ -25,4 +31,18 @@ function listClassesInDirectory(string $directory): iterable
     foreach ((new ClassReflector($sourceLocator))->getAllClasses() as $class) {
         yield $class->getName();
     }
+}
+
+/**
+ * get a list of all classes in the given direcotory.
+ *
+ * Based on: https://github.com/Roave/BetterReflection/blob/396a07c9d276cb9ffba581b24b2dadbb542d542e/demo/parsing-whole-directory/example2.php.
+ * 
+ * @param  string $directory
+ *
+ * @return iterable
+ */
+function listClassesInDirectory(string $directory): iterable
+{
+    return listClassesInDirecories($directory);
 }
